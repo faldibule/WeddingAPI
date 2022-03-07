c1c92d155bd4cfd13c9132e481085c64f2b262ae
faldibule <faldibule@gmail.com>
2022-03-07 13:41:17 +0700

    fix: some bugs

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ dotenv.config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
 require('./utils/Connection')
 const postRoute = require('./route/postRouter')
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.json({
